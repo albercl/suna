d547c71a91dd35b7e86bdf0a1621b23df8ee200f
albercl <alber@albercl.dev>
2022-10-09 18:46:07 +0200

    Winston config done (have to encapsulate code)

--- src/Index.ts
+++ src/Index.ts
@@ -1,1 +1,35 @@
-console.log("Debug is working now!")
+import { createLogger, format, transports } from 'winston';
+const { printf } = format;
+
+let logger_format = printf((info) => {
+    let msg = `${info.timestamp} [${info.level}] ${info.message}`;
+
+    let splat = Symbol('splat');
+    if (info.metadata) {
+        msg += '\n' + JSON.stringify(info.metadata, null, 4);
+    }
+
+    return msg;
+});
+
+let logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.prettyPrint({
+            colorize: true,
+        }),
+        format.metadata(),
+        format.timestamp({
+            format: 'YYYY-MM-DD hh:mm:ss',
+        }),
+        logger_format
+    ),
+    transports: [new transports.Console()],
+});
+
+logger.info('Logger is working!', {
+    myObj: {
+        name: 'albercl',
+    },
+    string: 'str',
+});
