918095eada3564b430940fce1cb706b9e5b60a91
albercl <alber@albercl.dev>
2022-10-21 09:42:01 +0200

    Document LoggerFactory

--- src/LoggerFactory.ts
+++ src/LoggerFactory.ts
@@ -2,6 +2,11 @@ import { createLogger, format, Logger, transports } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 const { printf } = format;
 
+/**
+ * Class with a singleton patter implemented that
+ * allows returning a logger with the name of the file where
+ * the {@link getLogger} method was called
+ */
 export default class LoggerFactory {
     // Singleton
     private static instance: LoggerFactory;
@@ -20,9 +25,15 @@ export default class LoggerFactory {
 
     private constructor() {}
 
+    /**
+     * Creates a logger that prints the name of the file where this method
+     * was called
+     * @returns a new logger associated to the file
+     */
     public getLogger(): Logger {
         let stackTrace = new Error().stack!;
 
+        // Get the file where the logger was created
         let file = stackTrace.split('\n')[1].split('/').pop()!.split(':')[0];
 
         return createLogger({
